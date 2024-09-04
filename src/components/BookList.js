b61098b3d0092404b21e416bec946881586e4146
Eduard Schumacher <107512513+mijamoto911@users.noreply.github.com>
2024-09-04 13:15:36 +0200

    +

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+export const favouriteBooks = [
+  { id: 'id-1', name: 'JS for beginners' },
+  { id: 'id-2', name: 'React basics' },
+  { id: 'id-3', name: 'React Router overview' },
+];
+
+export const BookList = ({ books }) => {
+  return (
+    <ul>
+      {books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      })}
+    </ul>
+  );
+};
